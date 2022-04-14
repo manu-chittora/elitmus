981db9d3a8d87a8435e6de61c2d8666073ad70ed
Manu Chittora <70255533+manu-chittora@users.noreply.github.com>
2022-04-14 17:12:57 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,33 @@
+
+        var words1=["Q", "M", "W", "P"];
+        var words2=["HI", "MY", "IS", "DO"];
+        var words3=["WIN", "DIE", "MAN", "FLY"];
+        var words4=["TREE", "RICE", "GAME", "WORK"];
+        var words5=["EARTH", "LUNCH", "CHALK", "CHAIR"];
+        var words6=["ELITMUS", "BUCKET", "MIRROR", "ALMIRAH"];
+        var level=1;
+        if(level==1)
+        {
+            var x=document.getElementById("matrix");
+            x.innnerHTML="<h1>Hello</h1>"
+        }
+        else if(level==2)
+        {
+
+        }
+        else if(level==3)
+        {
+            
+        }
+        else if(level==4)
+        {
+            
+        }
+        else if(level==5)
+        {
+            
+        }
+        else if(level==6)
+        {
+            
+        }
